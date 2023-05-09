2f650079f51903a2e9815cebb0a1ee42e42bf168
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-09 18:16:28 +0500

    ma-app adeed

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+function Header (){
+    return (
+        <div className="myClass">
+        <h1>Welcome to my React app</h1>
+        <nav>
+            <ul className="list">
+                <li><a href="https://www.facebook.com">Home</a></li>
+                <li> <a href="https://www.facebook.com">About</a></li>
+                <li><a href="https://www.facebook.com">Products</a></li>
+                <li><a href="https://www.facebook.com">Contact</a></li>
+            </ul>
+        </nav>
+        </div>
+    );
+};
+
+export default Header;
